e4221a316ae1ac5a64ed2460ccf331214ea014d1
David Banks <dave@hoglet.com>
2016-06-05 09:17:29 +0100

    Build script now builds AVR and PIC versions
    Change-Id: Iac1ab32450b9ac632d5783cdffa750f47b42207e

--- build.sh
+++ build.sh
@@ -2,20 +2,39 @@
 
 rm -f *.rom
 
-echo Assembling
-#ca65 -l atomm2.a000.lst -o a000.o -DAVR atommc2.asm
-#ca65 -l atomm2.e000.lst -o e000.o -DAVR -D EOOO atommc2.asm
+BASE=atommc2-3.0
+
+AVR_A000_ROM=${BASE}-a000-avr.rom
+AVR_E000_ROM=${BASE}-e000-avr.rom
+PIC_A000_ROM=${BASE}-a000-pic.rom
+PIC_E000_ROM=${BASE}-e000-pic.rom
+
+
+echo Assembling AVR
+ca65 -l atomm2.a000.lst -o a000.o -DAVR atommc2.asm
+ca65 -l atomm2.e000.lst -o e000.o -DAVR -D EOOO atommc2.asm
+
+echo Linking AVR
+ld65 a000.o -o ${AVR_A000_ROM} -C atommc2-a000.lkr 
+ld65 e000.o -o ${AVR_E000_ROM} -C atommc2-e000.lkr 
+
+echo Removing AVR object files
+rm -f *.o
+
+echo Assembling PIC
 ca65 -l atomm2.a000.lst -o a000.o atommc2.asm
 ca65 -l atomm2.e000.lst -o e000.o -D EOOO atommc2.asm
 
-echo Linking
-ld65 a000.o -o atommc2-3.0-a000.rom -C atommc2-a000.lkr 
-ld65 e000.o -o atommc2-3.0-e000.rom -C atommc2-e000.lkr 
+echo Linking PIC
+ld65 a000.o -o ${PIC_A000_ROM} -C atommc2-a000.lkr 
+ld65 e000.o -o ${PIC_E000_ROM} -C atommc2-e000.lkr 
 
-echo Cleaning
+echo Removing PIC object files
 rm -f *.o
 
-echo Checksumming
-md5sum atommc2-3.0-a000.rom
-md5sum atommc2-3.0-e000.rom
+for i in ${AVR_A000_ROM} ${AVR_E000_ROM} ${PIC_A000_ROM} ${PIC_E000_ROM}
+do
+    truncate -s 4096 $i
+    md5sum $i
+done
 
