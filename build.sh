0d22fdc14ca9d01337b9f98711159394b112b6b1
David Banks <dave@hoglet.com>
2019-05-27 14:03:00 +0100

    Update build.sh to build ATM files as well as raw ROM files
    Change-Id: I7607bb83166fb6ee6bd4f1b393c2753b25c0077b

--- build.sh
+++ build.sh
@@ -15,8 +15,8 @@ ca65 -l atomm2.a000.lst -o a000.o -DAVR atommc2.asm
 ca65 -l atomm2.e000.lst -o e000.o -DAVR -D EOOO atommc2.asm
 
 echo Linking AVR
-ld65 a000.o -o ${AVR_A000_ROM} -C atommc2-a000.lkr 
-ld65 e000.o -o ${AVR_E000_ROM} -C atommc2-e000.lkr 
+ld65 a000.o -o ${AVR_A000_ROM} -C atommc2-a000.lkr
+ld65 e000.o -o ${AVR_E000_ROM} -C atommc2-e000.lkr
 
 echo Removing AVR object files
 rm -f *.o
@@ -26,8 +26,8 @@ ca65 -l atomm2.a000.lst -o a000.o atommc2.asm
 ca65 -l atomm2.e000.lst -o e000.o -D EOOO atommc2.asm
 
 echo Linking PIC
-ld65 a000.o -o ${PIC_A000_ROM} -C atommc2-a000.lkr 
-ld65 e000.o -o ${PIC_E000_ROM} -C atommc2-e000.lkr 
+ld65 a000.o -o ${PIC_A000_ROM} -C atommc2-a000.lkr
+ld65 e000.o -o ${PIC_E000_ROM} -C atommc2-e000.lkr
 
 echo Removing PIC object files
 rm -f *.o
@@ -35,6 +35,41 @@ rm -f *.o
 for i in ${AVR_A000_ROM} ${AVR_E000_ROM} ${PIC_A000_ROM} ${PIC_E000_ROM}
 do
     truncate -s 4096 $i
-    md5sum $i
 done
 
+mkdir -p AVR
+mkdir -p PIC
+rm -f AVR/*
+rm -f PIC/*
+
+xxd -r > AVR/ATMMC3A <<EOF
+00: 41 54 4d 4d 43 33 41 00 00 00 00 00 00 00 00 00
+10: 00 A0 00 A0 00 10
+EOF
+cat ${AVR_A000_ROM} >> AVR/ATMMC3A
+mv  ${AVR_A000_ROM} AVR/ATMMC3A.rom
+md5sum AVR/ATMMC3A.rom
+
+xxd -r > AVR/ATMMC3E <<EOF
+00: 41 54 4d 4d 43 33 45 00 00 00 00 00 00 00 00 00
+10: 00 E0 00 E0 00 10
+EOF
+cat ${AVR_E000_ROM} >> AVR/ATMMC3E
+mv  ${AVR_E000_ROM} AVR/ATMMC3E.rom
+md5sum AVR/ATMMC3E.rom
+
+xxd -r > PIC/ATMMC3A <<EOF
+00: 41 54 4d 4d 43 33 41 00 00 00 00 00 00 00 00 00
+10: 00 A0 00 A0 00 10
+EOF
+cat ${PIC_A000_ROM} >> PIC/ATMMC3A
+mv  ${PIC_A000_ROM} PIC/ATMMC3A.rom
+md5sum PIC/ATMMC3A.rom
+
+xxd -r > PIC/ATMMC3E <<EOF
+00: 41 54 4d 4d 43 33 45 00 00 00 00 00 00 00 00 00
+10: 00 E0 00 E0 00 10
+EOF
+cat ${PIC_E000_ROM} >> PIC/ATMMC3E
+mv  ${PIC_E000_ROM} PIC/ATMMC3E.rom
+md5sum PIC/ATMMC3E.rom
